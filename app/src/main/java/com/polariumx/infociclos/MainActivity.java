6ca2b1c53ac408402e7a75943d85dd1600ee1567
Oso <manuelluis736@gmail.com>
2021-12-14 09:48:41 +0100

    Main Activity Design

--- app/src/main/java/com/polariumx/infociclos/MainActivity.java
+++ app/src/main/java/com/polariumx/infociclos/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.polariumx.infociclos.databinding.ActivityMainBinding;
 
@@ -27,10 +29,30 @@ public class MainActivity extends AppCompatActivity {
         setupUI();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        runAnimations();
+    }
+
     private void setupUI(){
         binding.activityMainSignUpButton.setOnClickListener(toNavigate(SignUpActivity.class));
         binding.activityMainLogInButton.setOnClickListener(toNavigate(LogInActivity.class));
     }
+
+    private void runAnimations() {
+        Animation fallAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.item_animation_fall_down);
+        fallAnim.reset();
+        addAnimation(binding.activityMainOnboardingText, fallAnim);
+        addAnimation(binding.activityMainImage, fallAnim);
+        addAnimation(binding.activityMainButtonsLayout, fallAnim);
+    }
+
+    private void addAnimation(View v, Animation anim){
+        v.clearAnimation();
+        v.startAnimation(anim);
+    }
+
 
     private View.OnClickListener toNavigate(Class c){
         return new View.OnClickListener() {
